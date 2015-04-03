5d5858b96313fee4688d7a8d2f7ed77e1e29fd49
wbolger23 <walker.bolger@gmail.com>
2015-04-03 15:02:39 -0700

    Added 'Hello World' code

--- projectzero/config/routes.rb
+++ projectzero/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'hello/sayhi'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root :to => "hello#sayhi"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
